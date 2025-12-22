ae3ebe3b0e25d2cce46ff24347ba0e4bd261f47b
a111223201 <a111223201@mail.shu.edu.tw>
2025-12-22 22:42:34 +0800

    114_12_22

--- onlineshop/src/auction/Buyer.java
+++
@@ -1,8 +0,0 @@
-// Buyer.java
-package auction;
-
-public class Buyer extends auction.User {
-    public Buyer(String userId, String username) {
-        super(userId, username);
-    }
-}
--- onlineshop/src/auction/Watchable.java
+++
@@ -1,9 +0,0 @@
-// Watchable.java
-package auction;
-
-public interface Watchable {
-    void addWatcher(User user);
-    void removeWatcher(User user);
-    void notifyWatchers(String eventMessage);
-}
-
